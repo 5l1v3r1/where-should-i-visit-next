645e6ec0e23b334e7e3206d4802a68811ccd7c5e
Mert Emin Kalender <merteminkalender@gmail.com>
2014-01-06 19:17:12 -0800

    cctoken removed

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ post '/search' do
 	departure = @departure_date.match(/([\d]{2})\/([\d]{2})\/([\d]{4}) ([\d]{1,2}):([\d]{2}) (AM|PM)/)
 	arrival = @arrival_date.match(/([\d]{2})\/([\d]{2})\/([\d]{4}) ([\d]{1,2}):([\d]{2}) (AM|PM)/)
 
-	@results, @valid = getResults(params["city"], departure, arrival)
+	@results, @valid, @error_message = getResults(params["city"], departure, arrival)
 	
 	erb :index
 end
@@ -30,7 +30,7 @@ end
 def getResults(city, departure, arrival)
 
 	unless settings.city_list.has_key? city
-		return [nil, false]
+		return [nil, false, '']
 	end
 
 	d_date = "#{departure[3]}-#{departure[1]}-#{departure[2]}"
@@ -44,9 +44,10 @@ def getResults(city, departure, arrival)
 	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 	request = Net::HTTP::Post.new(uri.request_uri)
 	request.body = '[,[[,"ad","[,[,[[,[\"'+city+'\"],,\"'+d_date+'\",,[,'+"#{d_hour}"+',1440],,,0],[,,[\"'+city+'\"],\"'+a_date+'\",,,[,'+"#{a_hour}"+',1260],,0]]],[,25.725122085289655,-128.82421875,48.51982981873451,-66.17578125],4]","1087832005431478",3]],[,[[,"b_al","pd:69"],[,"b_ahr","pd:s"],[,"b_al","sr:72"],[,"b_ahr","sr:s"],[,"b_al","fs:51"],[,"b_ahr","fs:s"],[,"b_lr","13:217"],[,"b_lr","19:0"],[,"b_lr","13:219"],[,"b_lr","3:351"],[,"b_qu","0"],[,"b_qc","1"]]]]'
+	#request.body = '[,[[,"ad","[,[,[[,[\"'+city+'\"],,\"'+d_date+'\",,[,'+"#{d_hour}"+',1440],,,0],[,,[\"'+city+'\"],\"'+a_date+'\",,,[,'+"#{a_hour}"+',1260],,0]]],[,-52.895362126290784,176.578125,79.0360206117436,67.171875],2]","1663421298220535",37]],[,[[,"b_al","fs:3377"],[,"b_qu","0"],[,"b_qc","4"]]]]'
 	#request.body = '[,[[,"ad","[,[,[[,[\"SEA\"],,\"2014-01-24\",,[,1020,1440],,,0],[,,[\"SEA\"],\"2014-01-26\",,,[,900,1260],,0]]],[,-53.31746531011099,176.2265625,78.90148328853348,66.8203125],4]","1272831854995598",45]],[,[[,"b_al","pd:69"],[,"b_ahr","pd:s"],[,"b_lr","15:24"],[,"b_lr","8:24"],[,"b_ca","50:525833"],[,"b_lr","15:17"],[,"b_qu","0"],[,"b_qc","1"]]]]'
 	#request.body = '[,[[,"ad","[,[,[[,[\"SEA\"],,\"2014-01-24\",,[,1020,1440],,,0],[,,[\"SEA\"],\"2014-01-26\",,,[,900,1260],,0]]],[,-52.895362126290784,176.578125,79.0360206117436,67.171875],2]","1663421298220535",37]],[,[[,"b_al","fs:3377"],[,"b_qu","0"],[,"b_qc","4"]]]]'
-	request['x-gwt-cctoken'] = 'ADS25WPi-GfaIIVeBylmuBqbprTRdBzWeCgzxVsE0gV44kxX2oKcTPbuqm0qADNzPjlyjcWQglZmlfjhOR4Y-PM4PrEZny17uJ9QnN0E4a9XQ3EP5UnhTvpDpZNkEglTuRBB-N1mAAB4tlBfcSMgK1v0zosr9k8pxguCjduBPYQPzjxPRCY'
+	#request['x-gwt-cctoken'] = 'ADS25WPi-GfaIIVeBylmuBqbprTRdBzWeCgzxVsE0gV44kxX2oKcTPbuqm0qADNzPjlyjcWQglZmlfjhOR4Y-PM4PrEZny17uJ9QnN0E4a9XQ3EP5UnhTvpDpZNkEglTuRBB-N1mAAB4tlBfcSMgK1v0zosr9k8pxguCjduBPYQPzjxPRCY'
 	request['x-gwt-module-base'] = 'https://www.google.com/flights/static/'
 	request['x-gwt-permutation'] = 'A282E654365BD08FA0ADED93950EF2D6'
 	response = http.request(request)
@@ -54,8 +55,8 @@ def getResults(city, departure, arrival)
 	body = response.body
 	
 	#body = File.read('sample.txt')
-	if body.include? "302"
-		return [nil, false]
+	unless response.code != 200
+		return [nil, false, "#{response.code} - #{response.message}"]
 	end
 
 	begin
@@ -82,10 +83,10 @@ def getResults(city, departure, arrival)
 		end
 		final_results.sort_by! { |r| r["price"]}
 		#puts (final_results).inspect
-		[final_results, true]
+		[final_results, true, '']
 
-	rescue Exception
-		[nil, false]
+	rescue Exception => e
+		[nil, false, body]
 	end
 
 end
